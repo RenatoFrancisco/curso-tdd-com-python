d3d5512012a34bb5ef17b0c3f3b83476caf3d59f
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-03-21 18:40:19 -0300

    Criando uma execeção ao negócio

--- tests/test_leilao.py
+++ tests/test_leilao.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from src.leilao.dominio import Usuario, Lance, Leilao
+from src.leilao.excecoes import LanceInvalido
 
 
 class TestLeilao(TestCase):
@@ -23,7 +24,7 @@ class TestLeilao(TestCase):
 
     def test_nao_deve_permitir_propor_lance_em_ordem_decrescente(self):
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             yuri = Usuario('yuri', 500.0)
             lance_do_yuri = Lance(yuri, 100.0)
 
@@ -72,7 +73,7 @@ class TestLeilao(TestCase):
 
     def test_nao_deve_permitir_lance_caso_usuario_seja_o_mesmo(self):
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             self.leilao.propoe(self.lance_do_gui)
             self.leilao.propoe(self.lance_do_gui)
 
